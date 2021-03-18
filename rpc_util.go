964f1a95947eff088e2d92fc54f8353c31736511
tspiridonova <tspiridonova@ozon.ru>
2021-03-18 19:04:30 +0300

    with *[]byte interface

--- rpc_util.go
+++ rpc_util.go
@@ -757,12 +757,12 @@ func recv(p *parser, c baseCodec, s *transport.Stream, dc Decompressor, m interf
 	if err != nil {
 		return err
 	}
-	if _, ok := m.(*io.PipeWriter); ok {
-		defer func() {
-			m.(*io.PipeWriter).Close()
-		}()
-		_, err = m.(*io.PipeWriter).Write(d)
-		return err
+	if _, ok := m.(*[]byte); ok {
+		*m.(*[]byte) = d
+		if payInfo != nil {
+			payInfo.uncompressedBytes = d
+		}
+		return nil
 	}
 	if err = c.Unmarshal(d, m); err != nil {
 		return status.Errorf(codes.Internal, "grpc: failed to unmarshal the received message %v", err)
